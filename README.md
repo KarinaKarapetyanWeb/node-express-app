6a75e9e1406439236a22cc76fec45b2f6749d139
Karina <karina_karapetyan@mail.ru>
2023-08-27 18:09:09 +0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # node-express-app
+
+Учебный проект по Node.js 
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { App } from './app';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { ILogger } from './logger/logger.interface';
+import { LoggerService } from './logger/logger.service';
+import { TYPES } from './types';
+import { UserController } from './users/users.controller';
+import { PrismaService } from './database/prisma.service';
+import { IUserController } from './users/users.controller.interface';
+import { UserService } from './users/users.service';
+import { IUserService } from './users/users.service.interface';
+import { ConfigService } from './config/config.service';
+import { IConfigService } from './config/config.service.interface';
+
+export interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
+}
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserService>(TYPES.UserService).to(UserService);
+	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
+	bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
+	bind<App>(TYPES.Application).to(App);
+});
+
+async function bootstrap(): Promise<IBootstrapReturn> {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	await app.init();
+	return { appContainer, app };
+}
+
+export const boot = bootstrap();
